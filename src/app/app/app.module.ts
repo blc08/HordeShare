8e160b70a43e8cf8e723dfc6a8b3742e06be4591
Szabolcs Biro <biroszabolcs208@gmail.com>
2025-04-13 18:44:14 +0200

    feat: Create login page

--- src/app/app/app.module.ts
+++ src/app/app/app.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
-// Angular Material Imports
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet, RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressBarModule,
+    MatCardModule,
+    RouterLink
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  isLoading: boolean = false;
+  loginError: string = '';
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(6)
+      ]]
+    });
+  }
+
+  onSubmit() {
+    this.login();
+  }
+
+  login() {
+    this.loginError = '';
+    
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.isLoading = true;
+
+    const { email, password } = this.loginForm.value;
+
+    if (email === 'test@gmail.com' && password === 'testpw') {
+      // Simulate API call
+      setTimeout(() => {
+        localStorage.setItem('isLoggedIn', 'true');
+        this.isLoading = false;
+        void this.router.navigate(['/home']);
+      }, 3000);
+    } else {
+      this.loginError = 'Invalid email or password!';
+      this.isLoading = false;
+    }
+  }
 }
