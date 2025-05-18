6a602e24194ce33ae809deddf34a9cd06be5d829
Szabolcs Biro <biroszabolcs208@gmail.com>
2025-05-18 23:40:44 +0200

    feat: hosting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,7 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAnalytics } from "firebase/analytics";
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
@@ -20,4 +21,18 @@ export const appConfig: ApplicationConfig = {
       messagingSenderId: "90351200255", 
       measurementId: "G-TTE347MVGC" 
     })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
-};
+  };
+
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyD42Fn1dH74pBalu1z2xt2rrunXtUEQXuU",
+  //   authDomain: "hordeshare-5c7ad.firebaseapp.com",
+  //   projectId: "hordeshare-5c7ad",
+  //   storageBucket: "hordeshare-5c7ad.firebasestorage.app",
+  //   messagingSenderId: "90351200255",
+  //   appId: "1:90351200255:web:cc120cc0f832d5781a29ca",
+  //   measurementId: "G-EG8J0LH7EL"
+  // };
+  
+  // // Initialize Firebase
+  // const app = initializeApp(firebaseConfig);
+  // const analytics = getAnalytics(app);
