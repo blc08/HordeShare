a548a746326de58b154bd5df43342cd1fbda9844
Szabolcs Biro <biroszabolcs208@gmail.com>
2025-05-18 21:26:35 +0200

    feat: setup firebase and angularfire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,21 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()), 
+    provideFirebaseApp(() => initializeApp({ 
+      projectId: "hordeshare-5c7ad", 
+      appId: "1:90351200255:web:a065a2355bf422131a29ca", 
+      storageBucket: "hordeshare-5c7ad.firebasestorage.app", 
+      apiKey: "AIzaSyD42Fn1dH74pBalu1z2xt2rrunXtUEQXuU", 
+      authDomain: "hordeshare-5c7ad.firebaseapp.com", 
+      messagingSenderId: "90351200255", 
+      measurementId: "G-TTE347MVGC" 
+    })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
